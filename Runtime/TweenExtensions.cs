d679e8cd946618eca5e57f64466b3312db3e38b7
DavidFDev <account@davidfdev.com>
2021-10-12 22:39:23 +1100

    Added TweenY extension method.

--- Runtime/TweenExtensions.cs
+++ Runtime/TweenExtensions.cs
@@ -18,8 +18,16 @@ namespace DavidFDev.Tweening
             {
                 transform.position = new Vector3(x, transform.position.y, transform.position.z);
                 onUpdate(x);
-            },
-            onComplete);
+            }, onComplete);
+        }
+
+        public static Tween TweenY(this Transform transform, float start, float end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<float> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, y =>
+            {
+                transform.position = new Vector3(transform.position.x, y, transform.position.z);
+                onUpdate(y);
+            }, onComplete);
         }
     }
 }
