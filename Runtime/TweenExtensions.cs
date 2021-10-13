23199bbd13522a968ee2509814ec3690550afc36
DavidFDev <account@davidfdev.com>
2021-10-13 17:06:20 +1100

    Added extension methods for tweening object rotations.

--- Runtime/TweenExtensions.cs
+++ Runtime/TweenExtensions.cs
@@ -141,5 +141,161 @@ namespace DavidFDev.Tweening
                 onUpdate?.Invoke(z);
             }, onComplete);
         }
+
+        /// <summary>
+        ///     Create a new instance that tweens the world rotation of an object's transform (euler).
+        /// </summary>
+        /// <inheritdoc cref="Tween.Create{T}(T, T, float, LerpFunction{T}, EasingFunction, bool, Action{T}, Action)"/>
+        public static Tween TweenRotation(this Transform transform, Vector2 start, Vector2 end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<Vector2> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, v =>
+            {
+                transform.eulerAngles = new Vector3(v.x, v.y, transform.eulerAngles.z);
+                onUpdate?.Invoke(v);
+            }, onComplete);
+        }
+
+        /// <summary>
+        ///     Create a new instance that tweens the world rotation of an object's transform (euler).
+        /// </summary>
+        /// <inheritdoc cref="Tween.Create{T}(T, T, float, LerpFunction{T}, EasingFunction, bool, Action{T}, Action)"/>
+        public static Tween TweenRotation(this Transform transform, Vector3 start, Vector3 end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<Vector3> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, v =>
+            {
+                transform.eulerAngles = v;
+                onUpdate?.Invoke(v);
+            }, onComplete);
+        }
+
+        /// <summary>
+        ///     Create a new instance that tweens the world rotation of an object's transform.
+        /// </summary>
+        /// <inheritdoc cref="Tween.Create{T}(T, T, float, LerpFunction{T}, EasingFunction, bool, Action{T}, Action)"/>
+        public static Tween TweenRotation(this Transform transform, Quaternion start, Quaternion end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<Quaternion> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, q =>
+            {
+                transform.rotation = q;
+                onUpdate?.Invoke(q);
+            }, onComplete);
+        }
+
+        /// <summary>
+        ///     Create a new instance that tweens the world x rotation of an object's transform (euler).
+        /// </summary>
+        /// <inheritdoc cref="Tween.Create{T}(T, T, float, LerpFunction{T}, EasingFunction, bool, Action{T}, Action)"/>
+        public static Tween TweenRotatationX(this Transform transform, float start, float end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<float> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, x =>
+            {
+                transform.eulerAngles = new Vector3(x, transform.eulerAngles.y, transform.eulerAngles.z);
+                onUpdate?.Invoke(x);
+            }, onComplete);
+        }
+
+        /// <summary>
+        ///     Create a new instance that tweens the world y rotation of an object's transform (euler).
+        /// </summary>
+        /// <inheritdoc cref="Tween.Create{T}(T, T, float, LerpFunction{T}, EasingFunction, bool, Action{T}, Action)"/>
+        public static Tween TweenRotatationY(this Transform transform, float start, float end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<float> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, y =>
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, y, transform.eulerAngles.z);
+                onUpdate?.Invoke(y);
+            }, onComplete);
+        }
+
+        /// <summary>
+        ///     Create a new instance that tweens the world z rotation of an object's transform (euler).
+        /// </summary>
+        /// <inheritdoc cref="Tween.Create{T}(T, T, float, LerpFunction{T}, EasingFunction, bool, Action{T}, Action)"/>
+        public static Tween TweenRotatationZ(this Transform transform, float start, float end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<float> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, z =>
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, z);
+                onUpdate?.Invoke(z);
+            }, onComplete);
+        }
+
+        /// <summary>
+        ///     Create a new instance that tweens the world rotation of an object's transform (euler).
+        /// </summary>
+        /// <inheritdoc cref="Tween.Create{T}(T, T, float, LerpFunction{T}, EasingFunction, bool, Action{T}, Action)"/>
+        public static Tween TweenLocalRotation(this Transform transform, Vector2 start, Vector2 end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<Vector2> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, v =>
+            {
+                transform.localEulerAngles = new Vector3(v.x, v.y, transform.localEulerAngles.z);
+                onUpdate?.Invoke(v);
+            }, onComplete);
+        }
+
+        /// <summary>
+        ///     Create a new instance that tweens the world rotation of an object's transform (euler).
+        /// </summary>
+        /// <inheritdoc cref="Tween.Create{T}(T, T, float, LerpFunction{T}, EasingFunction, bool, Action{T}, Action)"/>
+        public static Tween TweenLocalRotation(this Transform transform, Vector3 start, Vector3 end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<Vector3> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, v =>
+            {
+                transform.localEulerAngles = v;
+                onUpdate?.Invoke(v);
+            }, onComplete);
+        }
+
+        /// <summary>
+        ///     Create a new instance that tweens the world rotation of an object's transform.
+        /// </summary>
+        /// <inheritdoc cref="Tween.Create{T}(T, T, float, LerpFunction{T}, EasingFunction, bool, Action{T}, Action)"/>
+        public static Tween TweenLocalRotation(this Transform transform, Quaternion start, Quaternion end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<Quaternion> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, q =>
+            {
+                transform.localRotation = q;
+                onUpdate?.Invoke(q);
+            }, onComplete);
+        }
+
+        /// <summary>
+        ///     Create a new instance that tweens the world x rotation of an object's transform (euler).
+        /// </summary>
+        /// <inheritdoc cref="Tween.Create{T}(T, T, float, LerpFunction{T}, EasingFunction, bool, Action{T}, Action)"/>
+        public static Tween TweenLocalRotatationX(this Transform transform, float start, float end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<float> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, x =>
+            {
+                transform.localEulerAngles = new Vector3(x, transform.localEulerAngles.y, transform.localEulerAngles.z);
+                onUpdate?.Invoke(x);
+            }, onComplete);
+        }
+
+        /// <summary>
+        ///     Create a new instance that tweens the world y rotation of an object's transform (euler).
+        /// </summary>
+        /// <inheritdoc cref="Tween.Create{T}(T, T, float, LerpFunction{T}, EasingFunction, bool, Action{T}, Action)"/>
+        public static Tween TweenLocalRotatationY(this Transform transform, float start, float end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<float> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, y =>
+            {
+                transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, y, transform.localEulerAngles.z);
+                onUpdate?.Invoke(y);
+            }, onComplete);
+        }
+
+        /// <summary>
+        ///     Create a new instance that tweens the world z rotation of an object's transform (euler).
+        /// </summary>
+        /// <inheritdoc cref="Tween.Create{T}(T, T, float, LerpFunction{T}, EasingFunction, bool, Action{T}, Action)"/>
+        public static Tween TweenLocalRotatationZ(this Transform transform, float start, float end, float duration, EasingFunction easingFunction = null, bool begin = true, Action<float> onUpdate = null, Action onComplete = null)
+        {
+            return Tween.Create(start, end, duration, easingFunction, begin, z =>
+            {
+                transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, z);
+                onUpdate?.Invoke(z);
+            }, onComplete);
+        }
     }
 }
